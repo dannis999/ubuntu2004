42ed9b568cab91755f1f10e7e2cb3ce63dcebbde
root <root@guest.vultr>
2022-08-13 08:34:59 +0000

    nos

---
+++ usr/lib/x86_64-linux-gnu/openblas-pthread/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.8")
+SET(OpenBLAS_INCLUDE_DIRS /usr/include/x86_64-linux-gnu/openblas-pthread/)
+SET(OpenBLAS_LIBRARIES /usr/lib/x86_64-linux-gnu/openblas-pthread//libopenblas.so)
